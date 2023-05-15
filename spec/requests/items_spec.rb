83b885f311412dd47ec03cf3db876905e247d280
thansetan <62317096+thansetan@users.noreply.github.com>
2023-05-15 19:07:29 +0700

    update test cases

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Items", type: :request do
-  
-  let!(:todo) {create(:todo)}
-  let!(:items) {create_list(:item, 20, todo_id: todo.id)}
-  let(:todo_id) {todo.id}
-  let(:id) {items.first.id}
+RSpec.describe 'Items', type: :request do
+  let(:user) { create(:user) }
+  let!(:todo) { create(:todo, created_by: user.id) }
+  let!(:items) { create_list(:item, 20, todo_id: todo.id) }
+  let(:todo_id) { todo.id }
+  let(:id) { items.first.id }
+  let(:headers) { valid_headers }
 
   describe 'GET /todos/:todo_id/items' do
-    before { get "/todos/#{todo_id}/items" }
+    before { get "/todos/#{todo_id}/items", params: {}, headers: }
 
     context 'when todo exists' do
       it 'returns status code 200' do
@@ -34,7 +35,7 @@ RSpec.describe "Items", type: :request do
   end
 
   describe 'GET /todos/:todo_id/items/:id' do
-    before { get "/todos/#{todo_id}/items/#{id}" }
+    before { get "/todos/#{todo_id}/items/#{id}", params: {}, headers: }
 
     context 'when todo item exists' do
       it 'returns status code 200' do
@@ -60,10 +61,10 @@ RSpec.describe "Items", type: :request do
   end
 
   describe 'POST /todos/:todo_id/items' do
-    let(:valid_attributes) { { name: 'Visit Narnia', done: false } }
+    let(:valid_attributes) { { name: 'Visit Narnia', done: false }.to_json }
 
     context 'when request attributes are valid' do
-      before { post "/todos/#{todo_id}/items", params: valid_attributes }
+      before { post "/todos/#{todo_id}/items", params: valid_attributes, headers: }
 
       it 'returns status code 201' do
         expect(response).to have_http_status(201)
@@ -71,7 +72,7 @@ RSpec.describe "Items", type: :request do
     end
 
     context 'when an invalid request' do
-      before { post "/todos/#{todo_id}/items", params: {} }
+      before { post "/todos/#{todo_id}/items", params: {}, headers: }
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
@@ -84,9 +85,9 @@ RSpec.describe "Items", type: :request do
   end
 
   describe 'PUT /todos/:todo_id/items/:id' do
-    let(:valid_attributes) { { name: 'Mozart' } }
+    let(:valid_attributes) { { name: 'Mozart' }.to_json }
 
-    before { put "/todos/#{todo_id}/items/#{id}", params: valid_attributes }
+    before { put "/todos/#{todo_id}/items/#{id}", params: valid_attributes, headers: }
 
     context 'when item exists' do
       it 'returns status code 204' do
@@ -113,7 +114,7 @@ RSpec.describe "Items", type: :request do
   end
 
   describe 'DELETE /todos/:id' do
-    before { delete "/todos/#{todo_id}/items/#{id}" }
+    before { delete "/todos/#{todo_id}/items/#{id}", params: {}, headers: }
 
     it 'returns status code 204' do
       expect(response).to have_http_status(204)
--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -1,96 +1,104 @@
 require 'rails_helper'
 
-RSpec.describe "Todos", type: :request do
-  
-  let!(:todos) {create_list(:todo, 10)} #if todos is empty, create 10 fake todo objects
-  let(:todo_id) {todos.first.id} #create todo_id var, and assign first todo id
+RSpec.describe 'Todos', type: :request do
+  let(:user) { create(:user) }
+  let!(:todos) { create_list(:todo, 10, created_by: user.id) } # if todos is empty, create 10 fake todo objects
+  let(:todo_id) { todos.first.id } # create todo_id var, and assign first todo id
 
+  let(:headers) { valid_headers }
   describe 'GET /todos' do
-    before {get '/todos'}
+    before { get '/todos', params: {}, headers: }
 
     it 'returns todos' do
-      expect(json).not_to be_empty #response body musn't be empty
-      expect(json.size).to eq(10) #must have 10 items
+      expect(json).not_to be_empty # response body musn't be empty
+      expect(json.size).to eq(10) # must have 10 items
     end
-    
+
     it 'returns status code 200' do
-      expect(response).to have_http_status(200) #return 200 OK status code
+      expect(response).to have_http_status(200) # return 200 OK status code
     end
   end
-  
-    describe 'GET /todos/:id' do
-      before {get "/todos/#{todo_id}"} #get todo with valid todo id
 
-      context 'when the record exists' do
-        it 'returns the todo' do
-          expect(json).not_to be_empty #response body musn't be empty
-          expect(json['id']).to eq(todo_id) #the todo id in response must match requested todo id
-        end
+  describe 'GET /todos/:id' do
+    before { get "/todos/#{todo_id}", params: {}, headers: } # get todo with valid todo id
 
-        it 'returns status code 200' do
-          expect(response).to have_http_status(200)
-        end
+    context 'when the record exists' do
+      it 'returns the todo' do
+        expect(json).not_to be_empty # response body musn't be empty
+        expect(json['id']).to eq(todo_id) # the todo id in response must match requested todo id
       end
 
-      context 'when the record doesn\'t exist' do
-        let(:todo_id) {100} #get todo with todo id = 100 (invalid)
-        
-        it 'returns status code 404' do
-          expect(response).to have_http_status(404) #return 404 not found status code
-        end
-        
-        it 'returns a not found message' do
-          expect(response.body).to match(/Couldn't find Todo/) #response body must be "Couldn't find Todo"
-        end
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
       end
     end
 
-    describe 'POST /todos' do
-      let(:valid_attributes) {{title: 'WOW KEREN', created_by: '1'}} #Example of valid todo input
+    context 'when the record doesn\'t exist' do
+      let(:todo_id) { 100 } # get todo with todo id = 100 (invalid)
 
-      context 'when the request is valid' do
-        before {post '/todos', params: valid_attributes}
-
-        it 'creates a todo' do
-          expect(json['title']).to eq('WOW KEREN') #Must return a JSON with the title matching user input title
-        end
-
-        it 'returns status code 201' do
-          expect(response).to have_http_status(201) #return 201 created status code
-        end
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404) # return 404 not found status code
       end
 
-      context 'when the request is invalid' do
-        before {post '/todos', params: {title: 'TIDAK KEREN'}} #Example of invalid todo input (created by is empty)
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Todo/) # response body must be "Couldn't find Todo"
+      end
+    end
+  end
 
-        it 'returns a validation failure message' do
-          expect(response.body)
-            .to match(/Validation failed: Created by can't be blank/) #response body must be "Validation failed: Created by can't be blank"
-        end
+  describe 'POST /todos' do
+    let(:valid_attributes) { { title: 'WOW KEREN', created_by: user.id.to_s }.to_json } # Example of valid todo input
+
+    context 'when the request is valid' do
+      before { post '/todos', params: valid_attributes, headers: }
+
+      it 'creates a todo' do
+        expect(json['title']).to eq('WOW KEREN') # Must return a JSON with the title matching user input title
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201) # return 201 created status code
       end
     end
 
-    describe 'PUT /todos/:id' do
-      let(:valid_attributes) {{title: 'Shopping'}} #edit todo title to "Shopping"
+    context 'when the request is invalid' do
+      let(:invalid_attributes) { { title: nil }.to_json }
+      # Example of invalid todo input (created by is empty)
+      before do
+        post '/todos', params: invalid_attributes, headers:
+      end
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
 
-      context 'when the record exists' do
-        before {put "/todos/#{todo_id}", params: valid_attributes}
-
-        it 'updates the record' do
-          expect(response.body). to be_empty #response body should be empty
-        end
-
-        it 'returns status code 204' do
-          expect(response).to have_http_status(204) #return 204 No Content status code
-        end
+      it 'returns a validation failure message' do
+        expect(json['message'])
+          .to match(/Validation failed: Title can't be blank/) # response body must be "Validation failed: Created by can't be blank"
       end
     end
+  end
 
-    describe 'DELETE /todos/:id' do
-      before {delete "/todos/#{todo_id}"}
+  describe 'PUT /todos/:id' do
+    let(:valid_attributes) { { title: 'Shopping' }.to_json } # edit todo title to "Shopping"
+
+    context 'when the record exists' do
+      before { put "/todos/#{todo_id}", params: valid_attributes, headers: }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty # response body should be empty
+      end
 
       it 'returns status code 204' do
-        expect(response).to have_http_status(204) #return 204 No Content status code
+        expect(response).to have_http_status(204) # return 204 No Content status code
       end
     end
+  end
+
+  describe 'DELETE /todos/:id' do
+    before { delete "/todos/#{todo_id}", params: {}, headers: }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204) # return 204 No Content status code
+    end
+  end
 end
