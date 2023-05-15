378eb61af94303d29b1f3cd27535b7ddb0779d71
thansetan <62317096+thansetan@users.noreply.github.com>
2023-05-15 19:07:13 +0700

    update so todos will be created for each user

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,13 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: [:show, :update, :destroy]
+  before_action :set_todo, only: %i[show update destroy]
 
   def index
-    @todos = Todo.all
+    @todos = current_user.todos
     json_response(@todos)
   end
 
   def create
-    @todo = Todo.create!(todo_params)
+    @todo = current_user.todos.create!(todo_params)
     json_response(@todo, :created)
   end
 
@@ -28,7 +28,7 @@ class TodosController < ApplicationController
   private
 
   def todo_params
-    params.permit(:title, :created_by) #allowed params are only title and created_by
+    params.permit(:title) # title is the only allowed param
   end
 
   def set_todo
