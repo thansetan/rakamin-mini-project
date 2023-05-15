11dac1faa310a1054fa20f0e0bf88c6dd812e40a
thansetan <62317096+thansetan@users.noreply.github.com>
2023-05-15 19:46:56 +0700

    small changes

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -6,11 +6,16 @@ module ExceptionHandler
   class InvalidToken < StandardError; end
 
   included do
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       message = if e.model == 'Item'
                   "Couldn't find #{e.model} with 'id'=#{params['id']} in Todo with 'id=#{params['todo_id']}"
                 else
-                  e.message
+                  "Couldn't find #{e.model} with 'id'=#{params['id'] || params['todo_id']}"
                 end
       json_response({ message: }, :not_found)
     end
@@ -19,23 +24,14 @@ module ExceptionHandler
       json_response({ message: e.message }, :unprocessable_entity)
     end
 
-    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
-    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
-    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
-    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    private
 
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
+    def four_twenty_two(e)
+      json_response({ message: e.message }, :unprocessable_entity)
     end
-  end
 
-  private
-
-  def four_twenty_two(e)
-    json_response({ message: e.message }, :unprocessable_entity)
-  end
-
-  def unauthorized_request(e)
-    json_response({ message: e.message }, :unauthorized)
+    def unauthorized_request(e)
+      json_response({ message: e.message }, :unauthorized)
+    end
   end
 end
