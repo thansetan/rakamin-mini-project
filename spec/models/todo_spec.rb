a315eb627dfe7a28b917db0fcf60795f93f019fa
thansetan <62317096+thansetan@users.noreply.github.com>
2023-05-15 19:07:36 +0700

    small change

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Todo, type: :model do
 
   # make sure Todo model has 1:m relationship with Item model
-  it {should have_many(:items).dependent(:destroy) }
+  it {should have_many(:items).dependent(:destroy)}
   # make sure the title and created by are not empty
-  it {should validate_presence_of(:title) }
+  it {should validate_presence_of(:title)}
   it {should validate_presence_of(:created_by)}
 end
