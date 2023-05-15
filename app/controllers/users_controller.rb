b3ddd05e589599f184d1bfb26a501ac2b386b7aa
thansetan <62317096+thansetan@users.noreply.github.com>
2023-05-15 20:01:13 +0700

    prevent user to register with same email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :create
+  before_action :list_user
   def create
-    user = User.create!(user_params)
-    auth_token = AuthenticateUser.new(user.email, user.password).call
-    response = { message: Message.account_created, auth_token: }
-    json_response(response, :created)
+    if email_not_in_user_list?(user_params['email'])
+      user = User.create!(user_params)
+      auth_token = AuthenticateUser.new(user.email, user.password).call
+      response = { message: Message.account_created, auth_token: }
+      status = :created
+    else
+      response = { message: 'User with that email already exist!' }
+      status = :conflict
+    end
+    json_response(response, status)
   end
 
   private
@@ -16,5 +23,13 @@ class UsersController < ApplicationController
       :password,
       :password_confirmation
     )
+  end
+
+  def list_user
+    @user = User.all
+  end
+
+  def email_not_in_user_list?(email)
+    @user.where(email:).empty?
   end
 end
